f0ebdb4e516415bba95e497a1382341b9beeb855
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2022-04-07 17:13:17 +0200

    Add files via upload

---
+++ FileChooser.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
+
+public class FileChooser {
+
+    public static void main(String[] args) {
+
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        jfc.setDialogTitle("Select an image");
+        jfc.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("PNG and GIF images", "png", "gif");
+        jfc.addChoosableFileFilter(filter);
+
+        int returnValue = jfc.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            System.out.println(jfc.getSelectedFile().getPath());
+        }
+
+    }
+
+}
